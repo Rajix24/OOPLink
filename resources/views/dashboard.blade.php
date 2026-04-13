bbb2f5d0f5e03157f4eafb86998e382f82848de0
Younes Raji <rajix2100@gmail.com>
2026-04-13 17:03:23 -0400

    create bar in dashboard and links and logout btn

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,28 @@
-<x-layout>
+@extends('layout.dashboard-layout')
+@section('dashboard-links')
+<link rel="stylesheet" href="{{ asset('storage/css/main.css') }}">
+
+@endsection
+
+@section('dashboard-btn')
+<form method="post" action="{{ route('logout') }}">
+    @csrf
+    <button class=" logout" type="submit">
+    <img src="{{ asset('storage/logout.png') }}" class="logout-imag" width="40px" height="auto" alt="">    
+    </button>
+</form>
+@endsection
+@section('dashboard-action')
+
 <h1>wellcome {{ Auth::user()->name }}</h1>
 @auth
 <h1>user are auth {{ Auth::user()->id }}</h1>
 @endauth
-<form method="post" action="{{ route('logout') }}">
-    @csrf
-    <button class="btn btn-danger" type="submit">logout</button>
-</form>
 
 
 
 <a href="{{ route("article.index") }}" class="btn btn-primary"> see articles</a>
 <a href="{{ route("categories.index") }}" class="btn btn-primary"> categories</a>
 <a href="{{ route("tag.index") }}" class="btn btn-primary"> see tags</a>
-</x-layout>
+
+@endsection
