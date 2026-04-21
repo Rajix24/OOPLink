d0c5d02ba207b07053c9db39b252ad4d45e50200
Younes Raji <rajix2100@gmail.com>
2026-04-20 22:06:35 -0400

    I just add the all articles that in application

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -33,8 +33,7 @@
             <a href="">View all →</a>
         </div>
         <div class="admin-articles-container">
-            @foreach ($articles as $article)
-            
+            @foreach($articles as $article)
             <div class="Admin-article ">
                 <div class="admin-Article-info">
                     <div class="article-img-data">
@@ -43,11 +42,11 @@
                             <img width="80px" height="80px" src="{{ asset('storage/user _1.png') }}" alt="imag" class="admin-article-image">
                             @else
                             <img width="80px" height="80px" src="{{ asset('storage/'. $article->images[0]->image) }}" alt="imag" class="admin-article-image">
-                            @endi
+                            @endif
                         </div>
                         <div class="admin-article-title">
-                            <h4>title articel</h4>
-                            <p>publush at</p>
+                            <h4> <a href="">{{ $article->title }}</a></h4>
+                            <p>publush at: {{ $article->created_at }}</p>
                         </div>
                     </div>
                     <!-- <div class="Admin-article-information">
@@ -69,37 +68,6 @@
                 </div>
             </div>
             @endforeach
-
-            <div class="Admin-article ">
-                <div class="admin-Article-info">
-                    <div class="article-img-data">
-                        <div class="">
-                            <img width="80px" height="80px" src="{{ asset('storage/user _1.png') }}" alt="imag">
-                        </div>
-                        <div class="admin-article-title">
-                            <h4>title articel</h4>
-                            <p>publush at</p>
-                        </div>
-                    </div>
-                    <!-- <div class="Admin-article-information">
-                            <span class="">
-                                some information
-                            </span>
-                            <span class=" ">
-                                some information
-                            </span>
-                        </div> -->
-                </div>
-                <div class="Admin-article-actions">
-                    <a href="edit" class="action-link">Edit</a>
-                    <form action="#" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="action-link danger">Delete</button>
-                    </form>
-                </div>
-            </div>
-
         </div>
 
     </div>
