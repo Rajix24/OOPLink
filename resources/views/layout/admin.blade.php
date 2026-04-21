48b15da29b06f453da8fbedd096ed01f36d55778
Younes Raji <rajix2100@gmail.com>
2026-04-20 21:54:51 -0400

    I start admin page that is false that I do I will change branch

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,8 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @yield('title')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('storage/css/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('storage/css/layout.css') }}">
+    @yield('dashboard-links')
 </head>
+
 
 <body>
     <nav class="nav" id="nav">
@@ -40,9 +44,40 @@
             </div>
         </div>
     </nav>
-    <main>
-        <aside></aside>
+
+    <main class="main">
+        <div class="big">
+            @yield('dashboard-action')
+        </div>
+
+        <aside>
+            <div class="create-article">
+                <a class="create-article-side-bar" href="{{ route('article.create') }}">Create Article</a>
+            </div>
+            <div class="topic">
+                <h5>Recommended topics</h5>
+                <div class="topic-link">
+                    <a href="">Ai chnge the world</a>
+                </div>
+                <div class="topic-link">
+                    <a href="">Are Ai can make you better programmer</a>
+                </div>
+                <div class="topic-link">
+                    <a href="">php is death</a>
+                </div>
+            </div>
+            <div class="topic">
+                <h5>Categories</h5>
+
+            </div>
+
+            <div class="topic">
+            </div>
+        </aside>
     </main>
+    @yield('dashboard-script')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="{{ asset('storage/js/') }}"></script>
 </body>
 
 </html>
